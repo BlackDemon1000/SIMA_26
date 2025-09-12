5e775d77e76dfbcf65963020fb1b32403b46b1d6
BlackDemon1000 <gw1_10@gmx.de>
2025-09-12 19:20:30 +0200

    new microcontroller

--- Sima/src/main.cpp
+++ Sima/src/main.cpp
@@ -1,26 +1,22 @@
 #include <Arduino.h>
-
-#define S_RXD 16
-#define S_TXD 17
-
-
 #include <SCServo.h>
 
 SCSCL sc;
 
 void setup()
 {
-  Serial.begin(115200);
-  Serial1.begin(1000000, SERIAL_8N1, S_RXD, S_TXD);
-  sc.pSerial = &Serial1;
+  Serial.begin(1000000);   
+  sc.pSerial = &Serial;    
+
   delay(1000);
 }
 
 void loop()
 {
-  sc.WritePos(0xfe, 1000, 0, 1500);//All servos move at the max speed=1500, move to Position=1000
-  delay(754);//[(P1-P0)/V]*1000+100
-  
-  sc.WritePos(0xfe, 20, 0, 1500);//All servos move at the max speed=1500, move to Position=20
-  delay(754);//[(P1-P0)/V]*1000+100
+  sc.WritePos(0xfe, 1000, 0, 1500);
+  delay(754);
+
+  sc.WritePos(0xfe, 20, 0, 1500);
+  delay(754);
 }
+
