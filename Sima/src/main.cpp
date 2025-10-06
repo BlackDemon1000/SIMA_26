872aaf6c4c6dc92a3f47e55c0a63d60714aabe66
BlackDemon1000 <gw1_10@gmx.de>
2025-10-06 21:21:33 +0200

    nachregeln

--- Sima/src/main.cpp
+++ Sima/src/main.cpp
@@ -7,16 +7,33 @@ void setup()
 {
   Serial.begin(1000000);   
   sc.pSerial = &Serial;    
-
   delay(1000);
+}
+
+void moveAndCheck(uint8_t id, int ziel)
+{
+  sc.WritePos(id, ziel, 0, 1500);
+  delay(754); //temp
+
+  int ist = sc.ReadPos(id);
+  Serial.print("Servo ");
+  Serial.print(id);
+  Serial.print(" Ziel: ");
+  Serial.print(ziel);
+  Serial.print(" Ist: ");
+  Serial.println(ist);
+
+  //Nachregeln
+  if (abs(ist - ziel) > 10) { // tol anpassen
+    Serial.println("Abweichung erkannt, nachregeln...");
+    sc.WritePos(id, ziel, 0, 1500);
+  }
 }
 
 void loop()
 {
-  sc.WritePos(0xfe, 1000, 0, 1500);
-  delay(754);
-
-  sc.WritePos(0xfe, 20, 0, 1500);
-  delay(754);
+  moveAndCheck(4, 1000);
+  moveAndCheck(5, 20);
+  delay(1000);
 }
 
